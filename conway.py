4b3bd06a4b000d145a3748a9d75c988be0a4d470
Brandon T. Fields <brandon@cdated.com>
2013-12-14 13:33:38 -0500

    Add cell position on init test

--- conway.py
+++ conway.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python
+
 import unittest
 
 class Cell():
-    def __init__(self):
+    # postion(X, Y)
+    def __init__(self, state=False, position=(0,0)):
         pass
 
 class Cell_Tests(unittest.TestCase):
-    def test_cell_test(self):
-        pass
+    def test_cell_position_on_init(self):
+        cell = Cell(state=True, position=(1,4))
+        cell_new = Cell(state=True, position=(0,0))
+
+        self.assertNotEqual(cell.position, cell_new.position)
 
 
 if __name__ == '__main__':

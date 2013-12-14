700cc35eec1bd0cd453b8a9b80bf4c8cacd06416
Brandon T. Fields <brandon@cdated.com>
2013-12-14 13:24:29 -0500

    Added conway.py

---
+++ conway.py
@@ -0,0 +1,13 @@
+import unittest
+
+class Cell():
+    def __init__(self):
+        pass
+
+class Cell_Tests(unittest.TestCase):
+    def test_cell_test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()

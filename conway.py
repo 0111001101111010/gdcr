0b9373ea4c485a7f46b6c2d884e844915b6e4f43
Brandon T. Fields <brandon@cdated.com>
2013-12-14 13:38:43 -0500

    Add test state on init test

--- conway.py
+++ conway.py
@@ -5,7 +5,7 @@ import unittest
 class Cell():
     # postion(X, Y)
     def __init__(self, state=False, position=(0,0)):
-        pass
+        self.position = position
 
 class Cell_Tests(unittest.TestCase):
     def test_cell_position_on_init(self):
@@ -13,6 +13,11 @@ class Cell_Tests(unittest.TestCase):
         cell_new = Cell(state=True, position=(0,0))
 
         self.assertNotEqual(cell.position, cell_new.position)
+
+    def test_cell_state_on_init(self):
+        cell = Cell(state=True, position=(0,0))
+
+        self.assertEqual(cell.state, True)
 
 
 if __name__ == '__main__':
